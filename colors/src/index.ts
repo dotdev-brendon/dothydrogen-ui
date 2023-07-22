0ac70a1d81dc649e386ba0fae20842d780a59a4d
Brendon Nicholas <7224264+undefined@users.noreply.github.com>
2023-07-22 11:05:24 +0000

    update button variants

--- colors/src/index.ts
+++ colors/src/index.ts
@@ -4,6 +4,11 @@ const colors = {
   black: '#000000',
   white: '#FFFFFF',
 
+  brand: {
+    primary: '#000',
+    secondary: 'yellow',
+  },
+
   whiteAlpha: {
     50: 'rgba(255, 255, 255, 0.04)',
     100: 'rgba(255, 255, 255, 0.06)',
